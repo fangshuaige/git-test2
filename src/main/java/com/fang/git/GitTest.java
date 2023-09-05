8350c0cb4be2883cd6a9370043cc6280a44bf7db
fang <2090475800@qq.com>
2023-09-05 14:41:50 +0800

    first commit

---
+++ src/main/java/com/fang/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.fang.git;
+
+/**
+ * @author 方明佳
+ * @create 2023-09-05 14:23
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我是大帅哥");
+    }
+}
